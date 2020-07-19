81e753e436cfc3a7df5aa154e8d3e70539de85f3
Serena Gibbons <serenalgibbons@gmail.com>
2020-07-19 18:32:08 -0400

    Add Category field to Recipe table; Add data annotations to Recipe model

--- RecipesBlog/Models/Recipe.cs
+++ RecipesBlog/Models/Recipe.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using static System.Environment;
 
 namespace RecipesBlog.Models
@@ -7,12 +8,18 @@ namespace RecipesBlog.Models
     
     public partial class Recipe
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string Ingredients { get; set; }
+        [Required]
         public string Directions { get; set; }
         public string Source { get; set; }
+        public string Category { get; set; }
 
         public IEnumerable<string> IngredientsList
         {
