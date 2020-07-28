d9bd0aead177f82c961ad2e0a42c97f3e81627b9
Serena Gibbons <serenalgibbons@gmail.com>
2020-07-28 17:41:31 -0400

    Move create, delete, edit, and list (renamed Index) to Admin folder and refactor controllers and links

--- RecipesBlog/Controllers/RecipesController.cs
+++ RecipesBlog/Controllers/RecipesController.cs
@@ -20,13 +20,6 @@ namespace RecipesBlog.Controllers
             return View(db.Recipes.ToList());
         }
 
-        // GET: Recipes table view
-        [Authorize]
-        public ActionResult List()
-        {
-            return View(db.Recipes.ToList());
-        }
-
         // GET: Recipes/Recipe
         [Route("Recipes/Recipe/{name?}")]
         public ActionResult Recipe(int? id)
@@ -41,101 +34,6 @@ namespace RecipesBlog.Controllers
                 return HttpNotFound();
             }
             return View(recipe);
-        }
-
-        // GET: Recipes/Create
-        [Authorize]
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Recipes/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public ActionResult Create([Bind(Exclude = "Id")] Recipe recipe)
-        {
-            if (ModelState.IsValid)
-            {                
-                db.Recipes.Add(recipe);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-           
-            return View(recipe);
-        }
-
-        // GET: Recipes/Edit/5
-        [Authorize]
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Recipe recipe = db.Recipes.Find(id);
-            if (recipe == null)
-            {
-                return HttpNotFound();
-            }
-            return View(recipe);
-        }
-
-        // POST: Recipes/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public ActionResult Edit([Bind(Include = "Id,Name,Description,Ingredients,Directions,Source,Category,Image_URL")] Recipe recipe)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(recipe).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(recipe);
-        }
-
-        // GET: Recipes/Delete/5
-        [Authorize]
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Recipe recipe = db.Recipes.Find(id);
-            if (recipe == null)
-            {
-                return HttpNotFound();
-            }
-            return View(recipe);
-        }
-
-        // POST: Recipes/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        [Authorize]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Recipe recipe = db.Recipes.Find(id);
-            db.Recipes.Remove(recipe);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
 
         // GET: Recipes/Recipe
